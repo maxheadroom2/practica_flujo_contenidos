84869b5cd39d8c8b707fd4bf7dd21cb7e2fe0501
Victor Ruben Farias Rolon <sinteticbeat@hotmail.com>
2019-02-21 21:04:35 -0700

    modifico js

--- js.js
+++ js.js
@@ -2,9 +2,9 @@ function size() {
   var elmnt = document.getElementById("myDIV");
   var txt = "";
   txt += "Height, padding: " + elmnt.clientHeight + "px<br>";
-  txt += "Height, padding, border: " + elmnt.offsetHeight + "px<br>";
+  txt += "Height, padding, borde: " + elmnt.offsetHeight + "px<br>";
   txt += "Width, padding: " + elmnt.clientWidth + "px<br>";
-  txt += "Width, padding, border: " + elmnt.offsetWidth + "px";
+  txt += "Width, padding, borde: " + elmnt.offsetWidth + "px";
   document.getElementById("demo").innerHTML = txt;
   setInterval(size, 1000);
 }
